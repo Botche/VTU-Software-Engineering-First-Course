dac65e8f9681ccab8dba0eafb59fd9cf4989f3d8
Botche <gabrielpetkov622@gmail.com>
2020-10-08 21:15:47 +0300

    Add comments

--- L01_FirstStepsInProgramming/L01_FirstStepsInProgramming/StartUp.cs
+++ L01_FirstStepsInProgramming/L01_FirstStepsInProgramming/StartUp.cs
@@ -2,12 +2,22 @@
 
 namespace L01_FirstStepsInProgramming
 {
+    // Main class
     class StartUp
     {
+        // Main method where every program starts
         static void Main()
         {
-            int variable = 4;
+            // This is one line comment
 
+            /* 
+             * Multi line comment
+             */
+
+            // Declaration of variable with name 'number', type 'int' and value '4'
+            int number = 4;
+
+            // Writing on console - it will print 'Hello World!'
             Console.WriteLine("Hello World!");
         }
     }
