f8caa5cb8351f6ffcee44165ec469549a2480bd0
Gabriel <47752724+Botche@users.noreply.github.com>
2020-12-03 17:19:22 +0200

    Add method exercises

---
+++ L06_Methods/P01_Examples/StartUp.cs
@@ -0,0 +1,93 @@
+﻿namespace P01_Examples
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            //// ================================================================= //
+            //int lengthOfArray = int.Parse(Console.ReadLine());
+            //int[] array = new int[lengthOfArray];
+
+            //for (int index = 0; index < array.Length; index++)
+            //{
+            //    int number = int.Parse(Console.ReadLine());
+
+            //    array[index] = number;
+            //}
+
+            //PrintOneDimensionalArrayWithForLoop(array);
+
+            //// ================================================================= //
+            //int[] arrayFromNumbers = Console.ReadLine()
+            //    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            //    .Select(int.Parse)
+            //    .ToArray();
+
+            //PrintOneDimensionalArrayWithForLoop(arrayFromNumbers); 
+            //// ================================================================= //
+            //int[] arr = { 1, 2, 3, 4, 5, 6 };
+
+            // ================================================================= //
+            int lengthOfTwoDimensionalArray = int.Parse(Console.ReadLine());
+            int[,] twoDimensionalArrayOfNumbersWithForLoop
+                = new int[lengthOfTwoDimensionalArray, lengthOfTwoDimensionalArray];
+
+            for (int row = 0; row < twoDimensionalArrayOfNumbersWithForLoop.GetLength(0); row++)
+            {
+                for (int col = 0; col < twoDimensionalArrayOfNumbersWithForLoop.GetLength(1); col++)
+                {
+                    Console.Write($"[{row}, {col}] = ");
+                    int number = int.Parse(Console.ReadLine());
+
+                    twoDimensionalArrayOfNumbersWithForLoop[row, col] = number;
+                }
+
+                Console.WriteLine();
+            }
+
+            PrintArrayWithForLoop(twoDimensionalArrayOfNumbersWithForLoop);
+        }
+
+        private static void PrintArrayWithForLoop(int[,] array)
+        {
+            for (int row = 0; row < array.GetLength(0); row++)
+            {
+                for (int col = 0; col < array.GetLength(1); col++)
+                {
+                    Console.Write(array[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        // Example for overloading of methods
+        private static void PrintArrayWithForLoop(char[] array)
+        {
+            Console.WriteLine("The char array elements are: ");
+
+            for (int index = 0; index < array.Length; index++)
+            {
+                Console.Write(array[index] + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void PrintArrayWithForLoop(int[] array)
+        {
+            Console.WriteLine("The array elements are: ");
+
+            // 1 2 3 4 5 6
+            for (int index = 0; index < array.Length; index++)
+            {
+                Console.Write(array[index] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ L06_Methods/P03_Fibonacci/StartUp.cs
@@ -0,0 +1,79 @@
+﻿namespace P03_Fibonacci
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            #region Fibonacci without recursion
+            //int countOfFibonacciNumbers = int.Parse(Console.ReadLine());
+            //int[] fibonacciNumbers = new int[countOfFibonacciNumbers];
+
+            //int previousNumber = 1;
+            //int previousPreviousNumber = 1;
+
+            //fibonacciNumbers[0] = previousNumber;
+            //fibonacciNumbers[1] = previousPreviousNumber;
+
+            //for (int index = 2; index < fibonacciNumbers.Length; index++)
+            //{
+            //    int sumOfPreviousNumbers = previousNumber + previousPreviousNumber;
+            //    fibonacciNumbers[index] = sumOfPreviousNumbers;
+
+            //    previousPreviousNumber = previousNumber;
+            //    previousNumber = sumOfPreviousNumbers;
+            //}
+
+            //Console.WriteLine(string.Join(", ", fibonacciNumbers));
+            #endregion
+
+            #region Fibonacci with recursion
+            //int indexOfFibonacciNumber = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(FindFibonacciNumber(indexOfFibonacciNumber));
+
+            //// 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+
+            //// index 1 = 1
+            //// index 2 = 1
+            //// index 3 = 2
+            //// index 4 = 3
+            //// index 5 = 5
+            #endregion
+
+            #region Factorial
+            //int factorial = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(CalculateFactorial(factorial));
+
+            #endregion
+        }
+
+        private static int CalculateFactorial(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+
+            return CalculateFactorial(n - 1) * n;
+        }
+
+        private static int FindFibonacciNumber(int index)
+        {
+            if (index <= 2)
+            {
+                return 1;
+            }
+
+            int previousNumber = FindFibonacciNumber(index - 1);
+            int previousPreviousNumber = FindFibonacciNumber(index - 2);
+
+            Console.WriteLine($"Previous number: {previousNumber} at index: {index}");
+            Console.WriteLine($"Previous previous number: {previousPreviousNumber} at index: {index}");
+
+            return previousNumber + previousPreviousNumber;
+        }
+    }
+}
