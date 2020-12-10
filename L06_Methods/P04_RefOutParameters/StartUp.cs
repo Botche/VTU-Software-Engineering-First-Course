46ef78704ff1c14555a12a0bbf70bd8892978774
Gabriel <47752724+Botche@users.noreply.github.com>
2020-12-10 18:04:55 +0200

    Add try catch and string methods exercises

---
+++ L06_Methods/P04_RefOutParameters/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace P04_RefOutParameters
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string text = "0";
+
+            // int number = int.Parse(text);
+
+            bool isParsed = int.TryParse(text, out int number);
+
+            Console.WriteLine("My program is awesome");
+            Console.WriteLine(isParsed);
+            Console.WriteLine(number);
+        }
+    }
+}
---
+++ L06_Methods/P05_CustomTryParse/StartUp.cs
@@ -0,0 +1,69 @@
+﻿namespace P05_CustomTryParse
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            // try - catch || finally
+
+            // try - catch - finally
+
+            #region TryCatch
+            //try
+            //{
+            //    string text = "";
+
+            //    if (string.IsNullOrWhiteSpace(text) == true)
+            //    {
+            //        throw new ArgumentNullException();
+            //    }
+
+            //    int number = int.Parse(text);
+
+            //    Console.WriteLine(number);
+            //}
+            //catch (ArgumentNullException)
+            //{
+            //    Console.WriteLine("Argument was null");
+            //}
+            //catch (FormatException)
+            //{
+            //    Console.WriteLine("Format is not correct");
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+            #endregion
+
+            IntTryParse("asd", out int number);
+
+            Console.WriteLine(number);
+        }
+
+        private static bool IntTryParse(string text, out int number)
+        {
+            number = default;
+            bool isParsed = false;
+
+            try
+            {
+                number = int.Parse(text);
+
+                isParsed = true;
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Parsing was not successful");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error");
+            }
+
+            return isParsed;
+        }
+    }
+}
